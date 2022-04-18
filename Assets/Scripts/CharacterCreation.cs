2581a7e8bb3c5130facc4e1ad8b4516d4dba964a
ZitaRR <Haeggqvist1998@hotmail.com>
2022-04-18 19:28:53 +0200

    Character creation foundation - Implemented basic character creation - Sprites for character (beards, hair etc) - Scrips neccessary for character creation

---
+++ Assets/Scripts/CharacterCreation.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterCreation : MonoBehaviour
+{
+    private int HairIndex
+    {
+        get => hairIndex;
+        set
+        {
+            if (value < 0)
+            {
+                hairIndex = hair.Length - 1;
+            }
+            else if (value >= hair.Length)
+            {
+                hairIndex = 0;
+            }
+            else
+            {
+                hairIndex = value;
+            }
+
+            activeHair.sprite = hair[hairIndex];
+        }
+    }
+    private int BeardIndex
+    {
+        get => beardIndex;
+        set
+        {
+            if (value < 0)
+            {
+                beardIndex = beards.Length - 1;
+            }
+            else if (value >= beards.Length)
+            {
+                beardIndex = 0;
+            }
+            else
+            {
+                beardIndex = value;
+            }
+
+            activeBeard.sprite = beards[beardIndex];
+        }
+    }
+
+    [Header("Sprites")]
+    [SerializeField]
+    private Sprite[] hair;
+    [SerializeField]
+    private Sprite[] beards;
+
+    [Header("Misc")]
+    [SerializeField]
+    private TMP_Dropdown dropdown;
+    [SerializeField]
+    private ColorPicker colorPicker;
+    [SerializeField]
+    private TextMeshProUGUI customizationHeader;
+
+    private Image activeHair;
+    private Image activeBeard;
+    private Image activeEyes;
+    private Image activeHead;
+    private int hairIndex;
+    private int beardIndex;
+
+    private void Start()
+    {
+        activeHair = GameObject.FindGameObjectWithTag("Hair").GetComponent<Image>();
+        activeBeard = GameObject.FindGameObjectWithTag("Beard").GetComponent<Image>();
+        activeEyes = GameObject.FindGameObjectWithTag("Eyes").GetComponent<Image>();
+        activeHead = GameObject.FindGameObjectWithTag("Head").GetComponent<Image>();
+
+        dropdown.onValueChanged.AddListener(SetCustomizationOption);
+
+        HairIndex = 0;
+        BeardIndex = 0;
+
+        SetCustomizationOption(0);
+    }
+
+    public void SetCustomizationIndex(int index = 1)
+    {
+        switch (colorPicker.Image.name)
+        {
+            case "Hair":
+                HairIndex += index;
+                break;
+            case "Beard":
+                BeardIndex += index;
+                break;
+        }
+    }
+
+    public void SetCustomizationOption(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                colorPicker.Attach(activeHead);
+                customizationHeader.transform.parent.gameObject.SetActive(false);
+                break;
+            case 1:
+                colorPicker.Attach(activeHair);
+                customizationHeader.transform.parent.gameObject.SetActive(true);
+                break;
+            case 2:
+                colorPicker.Attach(activeEyes);
+                customizationHeader.transform.parent.gameObject.SetActive(false);
+                break;
+            case 3:
+                colorPicker.Attach(activeBeard);
+                customizationHeader.transform.parent.gameObject.SetActive(true);
+                break;
+        }
+
+        char[] chars = colorPicker.Image.name.ToUpper().ToCharArray();
+        customizationHeader.text = string.Join(" ", chars);
+    }
+}
---
+++ Assets/Scripts/ColorPicker.cs
@@ -0,0 +1,42 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPicker : MonoBehaviour
+{
+    public Image Image => image;
+
+    [SerializeField]
+    private TextMeshProUGUI header;
+    [SerializeField]
+    private Slider red;
+    [SerializeField]
+    private Slider green;
+    [SerializeField]
+    private Slider blue;
+
+    private Image image;
+
+    private void Start()
+    {
+        red.onValueChanged.AddListener(delegate { UpdateColor(); });
+        green.onValueChanged.AddListener(delegate { UpdateColor(); });
+        blue.onValueChanged.AddListener(delegate { UpdateColor(); });
+    }
+
+    public void Attach(Image image)
+    {
+        this.image = image;
+        char[] chars = image.name.ToUpper().ToCharArray();
+        header.text = string.Join(" ", chars);
+
+        red.SetValueWithoutNotify(this.image.color.r);
+        green.SetValueWithoutNotify(this.image.color.g);
+        blue.SetValueWithoutNotify(this.image.color.b);
+    }
+
+    private void UpdateColor()
+    {
+        image.color = new Color(red.value, green.value, blue.value);
+    }
+}
