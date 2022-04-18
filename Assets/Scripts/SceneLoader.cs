b6176275e6ce8a55850bbe288cc03523b60228e5
ZitaRR <Haeggqvist1998@hotmail.com>
2022-04-18 21:48:19 +0200

    Foundation for scene loading

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class SceneLoader : MonoBehaviour
+{
+    private static string sceneName;
+
+    [Header("Settings")]
+    [SerializeField]
+    private float fadeDuration;
+
+    [Header("UI")]
+    [SerializeField]
+    private Slider loadingBar;
+    [SerializeField]
+    private TextMeshProUGUI status;
+
+    private CanvasGroup canvas;
+
+    private void Start()
+    {
+        canvas = GetComponent<CanvasGroup>();
+        canvas.alpha = 0f;
+
+        StartCoroutine(Load());
+    }
+
+    private IEnumerator Load()
+    {
+        yield return StartCoroutine(SetAlpha(1f, fadeDuration));
+        AsyncOperation loading = SceneManager.LoadSceneAsync(sceneName);
+        loading.allowSceneActivation = false;
+
+        while (!loading.isDone)
+        {
+            float progress = (loading.progress + .1f);
+            loadingBar.value = progress;
+            if (progress >= 1)
+            {
+                break;
+            }
+            yield return null;
+        }
+
+        yield return StartCoroutine(Unload(loading));
+    }
+
+    private IEnumerator Unload(AsyncOperation loading)
+    {
+        yield return StartCoroutine(SetAlpha(0f, fadeDuration));
+        loading.allowSceneActivation = true;
+    }
+
+    private IEnumerator SetAlpha(float alpha, float duration)
+    {
+        float start = canvas.alpha;
+        float time = 0;
+
+        while (time < duration)
+        {
+            canvas.alpha = Mathf.Lerp(start, alpha, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        canvas.alpha = alpha;
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        SceneLoader.sceneName = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+}
