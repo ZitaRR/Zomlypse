849a08d72e6de987b1583b48afcd8b33151268f7
ZitaRR <Haeggqvist1998@hotmail.com>
2022-06-17 15:28:44 +0200

    Character creation, layout finished

---
+++ Assets/Scripts/Helpers/EnsureInput.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zomlypse.Helpers
+{
+    public class EnsureInput : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_InputField input;
+
+        private Button button;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+        }
+
+        private void Start()
+        {
+            input.onValueChanged.AddListener((input) => Ensure(input));
+            Ensure(input.text);
+        }
+
+        private void Ensure(string input)
+        {
+            button.interactable = !string.IsNullOrEmpty(input);
+        }
+    }
+}
